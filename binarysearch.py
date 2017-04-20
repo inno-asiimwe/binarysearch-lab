94e56e7b3bc170bebf6218d02451cf4b2b43e875
Innocent Asiimwe <asiimwe@outlook.com>
2017-04-20 04:28:04 +0300

    implementing the search method of the Binarysearch class

--- binarysearch.py
+++ binarysearch.py
@@ -4,5 +4,24 @@ class BinarySearch(list):
         self.extend(list(range(b, a*b+1, b)))
         self.length = a
 
+    def search(self, value):
+        count = 0
+        found = False
+        first = 0
+        last = self.length - 1
 
-print(BinarySearch(20, 1))
+        while first <= last and not found:
+            mid = (first + last)//2
+
+            if self[mid] == value:
+                found = True
+            elif self[mid] < value:
+                first = mid + 1
+            else:
+                last = mid - 1
+
+        return {'count':count, 'index':mid} if found else {'count':count, 'index':-1}
+
+
+
+print(BinarySearch(40, 2).search(33))
